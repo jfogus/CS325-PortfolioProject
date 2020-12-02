3ab93fb00b2ab1d08fdd2b439500ee80513b2385
Josh Fogus <josh.fogus@gmail.com>
2020-12-02 13:34:57 -0800

    Queue scaffolding

---
+++ modules/Queue.py
@@ -0,0 +1,21 @@
+# Author:  Joshua Fogus
+# Course:  CS 325_400_F2020
+# Date:  December 02, 2020
+# Description:  Implements a FIFO Queue with limited functionality.
+
+
+class Queue:
+    def __init__(self):
+        self.__queue = []
+
+    def enqueue(self, val):
+        """ Adds a value onto the end of the queue. """
+        pass
+
+    def dequeue(self):
+        """ Removes and returns a value from the beginning of the stack. """
+        pass
+
+
+if __name__ == "__main__":
+    print("This is not meant to be run as a script. Please import this module.")
---
+++ unittests/QueueTest.py
@@ -0,0 +1,15 @@
+# Author:  Joshua Fogus
+# Course:  CS 325_400_F2020
+# Date:  December 02, 2020
+# Description:  Tests for the Queue.py module.
+
+import unittest
+from Queue import Queue
+
+
+class QueueTester(unittest):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
